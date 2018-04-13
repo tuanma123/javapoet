ab64538c1e8f2d43fb24691fd425c57b6c462123
Tuan Ma <tuanma123@gmail.com>
2018-04-13 13:40:59 -0700

    Added Javadoc comments.

--- src/test/java/com/squareup/javapoet/CommentsTest.java
+++ src/test/java/com/squareup/javapoet/CommentsTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class CommentsTest {
     private MethodSpec computeRange(String name, int from, int to, String op) {
         return MethodSpec.methodBuilder(name)
+                .addJavadoc("Multiplication in a loop")
                 .returns(int.class)
                 .addComment("For loop comment")
                 .addStatement("int result = 0")
@@ -21,7 +22,9 @@ public class CommentsTest {
     public void generateForLoopTest() {
         MethodSpec test = computeRange("multiply10to20", 10, 20, "*");
         System.out.println(test);
-        String expected = "int multiply10to20() {\n" +
+        String expected = "/**\n" +
+                " * Multiplication in a loop */\n" +
+                "int multiply10to20() {\n" +
                 "  // For loop comment\n" +
                 "  int result = 0;\n" +
                 "  for (int i = 10; i < 20; i++) {\n" +
@@ -30,5 +33,18 @@ public class CommentsTest {
                 "  return result;\n" +
                 "}\n";
         assertEquals(test.toString(), expected);
+    }
+
+    /**
+     * Multiplication in a loop
+     * @return
+     */
+    int multiply10to20() {
+        // For loop comment
+        int result = 0;
+        for (int i = 10; i < 20; i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
